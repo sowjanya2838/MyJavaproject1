c8dbaa436c19f28422292cffe52bfc3c99fafff7
sowjanya2838 <sowjanya2838@gmail.com>
2023-12-09 17:21:22 +0530

    First commit

---
+++ src/com/edubridge/app1/util/Calc.java
@@ -0,0 +1,15 @@
+package com.edubridge.app1.util;
+
+public class Calc {
+	public int findSquare(int num) {
+		return num*num;
+	}
+
+	public static void main(String[] args) {
+		
+			
+		}
+
+	}
+
+
