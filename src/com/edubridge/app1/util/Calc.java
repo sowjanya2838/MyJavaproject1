8d0839fc23aac8d5068e58a932d2b592af7289d5
sowjanya2838 <153358665+sowjanya2838@users.noreply.github.com>
2023-12-09 18:05:50 +0530

    Update Calc.java

--- src/com/edubridge/app1/util/Calc.java
+++ src/com/edubridge/app1/util/Calc.java
@@ -4,8 +4,9 @@ public class Calc {
 	public int findSquare(int num) {
 		return num*num;
 	}
-
-	public static void main(String[] args) {
+public int findCube(int num) {
+		return num*num;
+	 
 		
 			
 		}
